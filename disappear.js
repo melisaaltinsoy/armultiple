31ede3b104d4cbed268924e63b0cce928d46a608
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2019-01-10 21:18:41 +0100

    Update disappear.js

--- disappear.js
+++ disappear.js
@@ -1,1 +1,1 @@
-setTimeout(function(){ alert("WELCOME TO MY UNIVERSE, TO EXPLORE ME PLEASE FOLLOW THE ROUTE"); }, 3000);
+setTimeout(function(){ alert("WELCOME TO MY REALITY, CLICK AND EXPLORE."); }, 3000);
