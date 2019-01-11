f33c31391efc9f0302d17b2375ea94c792609442
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2019-01-11 16:15:16 +0100

    Add files via upload

---
+++ socket_sender.js
@@ -0,0 +1,29 @@
+initializeWebSocket();
+
+
+function initializeWebSocket() {
+            
+	if ("WebSocket" in window) {
+	   
+	   // Let us open a web socket
+	   var ws = new WebSocket("ws://localhost:7000/parallel/");
+		
+	   ws.onopen = function() {
+		  
+		  // Web Socket is connected, send data using send()
+		  alert("Socket Opened");
+		  ws.send("Hellooooo");
+	   };
+		
+	   ws.onclose = function() { 		  
+		  // websocket is closed.
+		  alert("Connection is closed..."); 
+	   };
+	} else {	  
+	   // The browser doesn't support WebSocket
+	   alert("WebSocket NOT supported by your Browser!");
+	}
+ }
+ 
+ 
+ //when i click on .... send a message with ws.send("Message to send");
